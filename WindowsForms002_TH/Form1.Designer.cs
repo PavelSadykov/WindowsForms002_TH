828281619fa64934b513cae41bf82e850f86500b
PavelSadykov <2439747@gmail.com>
2023-08-25 12:29:47 +0500

    Добавьте файлы проекта.

---
+++ WindowsForms002_TH/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsForms002_TH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inputNametextBox = new System.Windows.Forms.TextBox();
+            this.downLoadPicture = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(61, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 335);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMoveForRectangle);
+            // 
+            // inputNametextBox
+            // 
+            this.inputNametextBox.Location = new System.Drawing.Point(549, 84);
+            this.inputNametextBox.Name = "inputNametextBox";
+            this.inputNametextBox.Size = new System.Drawing.Size(144, 20);
+            this.inputNametextBox.TabIndex = 1;
+            // 
+            // downLoadPicture
+            // 
+            this.downLoadPicture.Location = new System.Drawing.Point(422, 43);
+            this.downLoadPicture.Name = "downLoadPicture";
+            this.downLoadPicture.Size = new System.Drawing.Size(121, 23);
+            this.downLoadPicture.TabIndex = 2;
+            this.downLoadPicture.Text = "DownLoad";
+            this.downLoadPicture.UseVisualStyleBackColor = true;
+            this.downLoadPicture.Click += new System.EventHandler(this.downLoadPicture_Click_1);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(422, 84);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(121, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click_1);
+            this.Save.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.downLoadPicture);
+            this.Controls.Add(this.inputNametextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox inputNametextBox;
+        private System.Windows.Forms.Button downLoadPicture;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ WindowsForms002_TH/Form1.cs
@@ -0,0 +1,155 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace WindowsForms002_TH
+{
+    public partial class Form1 : Form
+    {
+        private Point rectangleStartPoint;
+        private List<Rectangle> rectangles = new List<Rectangle>();
+        private List<string> names = new List<string>();
+
+        private bool isDrawing = false;
+        private Point startPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.MouseClick += pictureBox1_MouseClick;
+            pictureBox1.Paint += pictureBox1_Paint;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                rectangleStartPoint = e.Location;
+                pictureBox1.MouseMove += pictureBox1_MouseMoveForRectangle;
+                pictureBox1.MouseUp += pictureBox1_MouseUpForRectangle;
+
+            }
+
+        }
+        private void pictureBox1_MouseMoveForRectangle(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Refresh();
+            using (Graphics g = pictureBox1.CreateGraphics())
+            using (Pen pen = new Pen(Color.Blue, 2))
+            {
+                int x = Math.Min(rectangleStartPoint.X, e.X);
+                int y = Math.Min(rectangleStartPoint.Y, e.Y);
+                int width = Math.Abs(e.X - rectangleStartPoint.X);
+                int height = Math.Abs(e.Y - rectangleStartPoint.Y);
+
+                // Проверяем, чтобы прямоугольник не выходил за границы изображения
+                x = Math.Max(x, 0);
+                y = Math.Max(y, 0);
+                width = Math.Min(width, pictureBox1.Width - x);
+                height = Math.Min(height, pictureBox1.Height - y);
+
+                Rectangle rect = new Rectangle(x, y, width, height);
+
+                g.DrawRectangle(pen, rect);
+            }
+        }
+        private void pictureBox1_MouseUpForRectangle(object sender, MouseEventArgs e)
+        {
+            pictureBox1.MouseMove -= pictureBox1_MouseMoveForRectangle;
+            pictureBox1.MouseUp -= pictureBox1_MouseUpForRectangle;
+
+            Rectangle rect = new Rectangle(
+                Math.Min(rectangleStartPoint.X, e.X),
+                Math.Min(rectangleStartPoint.Y, e.Y),
+                Math.Abs(e.X - rectangleStartPoint.X),
+                Math.Abs(e.Y - rectangleStartPoint.Y));
+
+            rectangles.Add(rect);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            using (Pen pen = new Pen(Color.Blue, 2))
+            {
+                foreach (var rect in rectangles)
+                {
+                    e.Graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+
+        public object Interaction { get; private set; }
+
+
+        private void Save_Click_1(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "CSV Files|*.csv";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName, false, Encoding.UTF8))
+                    {
+                        writer.WriteLine("Name,RectangleX,RectangleY,RectangleWidth,RectangleHeight");
+
+                        for (int i = 0; i < rectangles.Count; i++)
+                        {
+                            Rectangle rect = rectangles[i];
+                            string name = names[i];
+                            writer.WriteLine($"{name},{rect.X},{rect.Y},{rect.Width},{rect.Height}");
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void inputNametextBox_TextChanged(object sender, EventArgs e)
+        {
+            string name = inputNametextBox.Text;
+            if (rectangles.Count > 0 && names.Count < rectangles.Count)
+            {
+                names.Add(name);
+                inputNametextBox.Clear();
+            }
+        }
+
+        //кнопка загрузки картинки
+        private void downLoadPicture_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = new Bitmap(openFileDialog.FileName);
+
+                    rectangles.Clear();
+                    names.Clear();
+                    inputNametextBox.Clear();
+
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+
+            pictureBox1.MouseClick -= pictureBox1_Click;
+
+            rectangleStartPoint = Cursor.Position;
+
+
+            pictureBox1.MouseUp += pictureBox1_MouseUpForRectangle;
+        }
+
+        
+    }
+}
